414d2f92f4e35fd92c6e4dea952f6bc202e72742
Alberto Defendi <1632-defealb@users.noreply.version.helsinki.fi>
2022-09-20 16:54:42 +0200

    Sitemap script.

---
+++ sitemap.sh
@@ -0,0 +1,29 @@
+#! /usr/bin/env bash
+sitemap="public/sitemap.xml"
+baseurl="https://https://defending1.github.io"
+timestamp=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+
+function s_url() {
+  echo "<url><loc>${baseurl}/$1<loc><lastmod>$timestamp</lastmod></url>"
+}
+
+robots=`cat <<EOF
+User-agent: *
+Allow: *
+Sitemap: $baseurl/sitemap.xml
+EOF
+`
+echo "$robots" > public/robots.txt
+
+header=`cat <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+EOF
+` 
+echo "$header" > $sitemap
+
+for file in $(find public -name "*.html" | sed -e 's/public\///'); do
+  echo $(s_url $file) >> $sitemap
+done
+
+printf "</urlset>\n" >> $sitemap
